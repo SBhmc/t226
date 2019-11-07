c7d075c6b127fb74640698cd53b3a859c727a431
jt <2039840744@qq.com>
2019-11-07 19:49:39 +0800

    本地仓库接收

---
+++ ssm/src/demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @author jt
+ * @site www.xiaomage.com
+ * @company xxx公司
+ * @create  2019-11-07 19:46
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("idea git demo");
+    }
+}
